18fb455abbe49e0e3976a2873ef3b9739fc97a3b
Krantikumar Mathapati <krantikumar3446@gmail.com>
2023-11-11 17:19:13 +0530

    Created header

---
+++ src/main.js
@@ -0,0 +1,42 @@
+// tailwind custom classes
+// const hamburger = document.querySelector(".hamburger");
+
+
+import { domainToUnicode } from "url";
+
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        pri: "hsl(233, 26%, 24%)",
+        sec: "hsl(233, 8%, 62%)",
+        gColorOne: "hsl(136, 65%, 51%)",
+      },
+    },
+  },
+};
+
+// screens: {
+//     mobile: "320px",
+
+//     tablet: "640px",
+//     // => @media (min-width: 640px) { ... }
+
+//     laptop: "1024px",
+//     // => @media (min-width: 1024px) { ... }
+
+//     desktop: "1280px",
+//     // => @media (min-width: 1280px) { ... }
+//   },
+
+// xs: "320px",
+//         sm: "480px",
+//         md: "768px",
+//         lg: "976px",
+//         xl: "1440px",
+
+// hamburger.addEventListener("click", () => {
+//   nav.classList.toggle("active");
+// });
+
+
